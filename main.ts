d01b1d2f776209495948aed6035575dd07fc6801
livhat11 <161827827+livhat11@users.noreply.github.com>
2024-03-01 11:21:47 +0100

    Startfiler for MakeCode-prosjektet

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+basic.forever(function () {
+    music.play(music.stringPlayable("- - - - - - - - ", 170), music.PlaybackMode.UntilDone)
+})
+basic.forever(function () {
+	
+})
